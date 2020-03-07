497801bf35f3c1e80ab307fc4d42fc42cbced8c4
Vladislav Chunikhin <vladislav.chunikhin.95@gmail.com>
2020-03-07 22:56:04 +0400

    develop, init commit

---
+++ src/main/kotlin/com/practice/knowledgelibrary/KnowledgeLibraryApplication.kt
@@ -0,0 +1,11 @@
+package com.practice.knowledgelibrary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KnowledgeLibraryApplication
+
+fun main(args: Array<String>) {
+    runApplication<KnowledgeLibraryApplication>(*args)
+}
---
+++ src/test/kotlin/com/practice/knowledgelibrary/KnowledgeLibraryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.practice.knowledgelibrary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KnowledgeLibraryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
